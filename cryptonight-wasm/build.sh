f92ad622d20ec22025e4e743f9a471b6d47c424b
good506 <good5068@sina.com>
2018-10-07 19:47:33 -0700

    init

---
+++ cryptonight-wasm/build.sh
@@ -0,0 +1,13 @@
+emcc crypto/*.c -Oz \
+    -s DISABLE_EXCEPTION_CATCHING=1 \
+    -s BINARYEN_ASYNC_COMPILATION=1 \
+    -s ALIASING_FUNCTION_POINTERS=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s WASM=1 \
+    -s BINARYEN=1 \
+    -s NO_EXIT_RUNTIME=1 \
+    -s ASSERTIONS=1 \
+    -s STACK_OVERFLOW_CHECK=1 \
+    -s EXPORTED_FUNCTIONS="['_cryptonight_hash']" \
+    --post-js ../web/lib/worker.js \
+    -o ../web/lib/cryptonight.js
